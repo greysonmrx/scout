1941dd00f0390b03328bde4a52d578fa11be7c82
Greyson Filho <greysonmrx@gmail.com>
2021-02-25 22:27:51 -0300

    feat: create radar component

---
+++ src/components/Radar/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Radar as Chart, ChartComponentProps } from 'react-chartjs-2';
+import { useTheme } from 'styled-components';
+
+const Radar: React.FC<ChartComponentProps> = ({ ...props }) => {
+  const theme = useTheme();
+
+  return (
+    <Chart
+      options={{
+        scale: {
+          ticks: {
+            display: false,
+          },
+          gridLines: {
+            lineWidth: 2,
+            color: theme.colors.lightGrey,
+          },
+          angleLines: {
+            lineWidth: 2,
+            color: theme.colors.lightGrey,
+          },
+          pointLabels: {
+            fontColor: theme.colors.darkGrey,
+            fontFamily: 'Exo 2',
+            fontSize: 16,
+          },
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export default Radar;
